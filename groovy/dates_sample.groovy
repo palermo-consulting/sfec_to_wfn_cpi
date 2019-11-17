7adc42305ac689ca0ce5b1a64b6a3b1f6616c139
John Palermo <37883476+palermo-consulting@users.noreply.github.com>
2019-11-17 11:49:42 -0500

    iFlow v0.0.4
    Updated the WhereClauseBuilder groovy script to handle full delta:
    
    FROM CompoundEmployee
    	WHERE ( last_modified_on > to_datetime(LastModifiedDate)
        AND fromDate = to_date(FromDate, 'YYYY-MM-DD')
        AND toDate   = to_date(ToDate, 'YYYY-MM-DD') )
    
    Also added parameter to Compound Employee API, in channel settings.  XML notation for reference is:
    <urn:param>
    <urn:name>queryMode</urn:name>    <urn:value>periodDelta</urn:value>
    </urn:param>

--- groovy/dates_sample.groovy
+++ groovy/dates_sample.groovy
@@ -3,9 +3,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 DateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd");
-Date date = new Date(); //Format Fri Nov 15 17:55:48 EST 2019
+Date date = new Date(); //Today's date in the format Fri Nov 15 17:55:48 EST 2019
 
 println(date.toString());
 
-String dateString = dateFormat.format(date); //Format 2019-11-15
-println(dateString);
+String today = dateFormat.format(date); //Format 2019-11-15
+println(today);
